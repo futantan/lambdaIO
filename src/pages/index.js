0e4f2588e72e5db1772c23739b88d58ac12524ff
futantan <luckytantanfu@gmail.com>
2019-03-20 22:23:56 +0800

    reorder component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import styled from "styled-components"
 
+const IndexPage = () => <Container>Hello world!</Container>
+
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 `
 
-const IndexPage = () => <Container>Hello world!</Container>
 export default IndexPage
