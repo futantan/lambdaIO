13bfe509b683f77a2a7d737645dbf663bb197606
futantan <luckytantanfu@gmail.com>
2019-08-20 08:36:39 +0800

    add header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
-const IndexPage = () => <Container>Hello world!</Container>
+const Header = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `}
+    render={data => <div>{data.site.siteMetadata.title}</div>}
+  />
+)
+
+const IndexPage = () => (
+  <Container>
+    Hello world!
+    <Header />
+  </Container>
+)
 
 const Container = styled.div`
   display: flex;
