cc12013bb6da6c3367a858dfc8e15184da83639b
futantan <luckytantanfu@gmail.com>
2019-09-01 11:29:09 +0800

    use css module

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
+import styles from './Layout.module.css'
 
-const Layout = ({ children }) => (
-  <div
-    style={{
-      'margin-left': 'auto',
-      'margin-right': 'auto',
-      padding: '42px 20px',
-      'max-width': '672px',
-    }}
-  >
-    {children}
-  </div>
-)
+const Layout = ({ children }) => <div className={styles.layout}>{children}</div>
 
 export default Layout
