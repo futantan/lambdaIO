5c56dfb6043885b5e589199b70bde5e6fe8781f1
futantan <luckytantanfu@gmail.com>
2020-03-04 20:31:07 +0800

    update image path

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react'
+import { Link } from 'gatsby'
 
 const Profile = () => (
   <Link to="/">
-    <div className='flex flex-row items-center'>
+    <div className="flex flex-row items-center">
       <img
-        className='w-16 mr-3 rounded-full'
-        src="https://avatars2.githubusercontent.com/u/6268441?s=460&v=4"
+        className="w-16 mr-3 rounded-full"
+        src="/images/avatar.png"
         alt="avatar"
       />
       <p>傅坦坦的个人博客</p>
--- src/components/WechatQrCode/WechatQrCode.jsx
+++ src/components/WechatQrCode/WechatQrCode.jsx
@@ -3,11 +3,7 @@ import style from './style.module.css'
 
 export const WechatQrCode = () => (
   <div className={style.wechat}>
-    <img
-      className="mb-2"
-      src="https://raw.githubusercontent.com/futantan/lambdaIO/master/assets/wechat.png"
-      alt="wechat"
-    />
+    <img className="mb-2" src="/images/wechat.png" alt="wechat" />
     <p className="text-center">欢迎关注公众号</p>
   </div>
 )
